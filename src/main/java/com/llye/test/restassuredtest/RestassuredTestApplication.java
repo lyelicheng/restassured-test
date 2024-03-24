ebbe9ba4b022fd5e646b978201bf1f74f0822118
joycelye <joycelye1991@gmail.com>
2024-03-24 21:46:30 +0800

    init

---
+++ src/main/java/com/llye/test/restassuredtest/RestassuredTestApplication.java
@@ -0,0 +1,13 @@
+package com.llye.test.restassuredtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestassuredTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestassuredTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/llye/test/restassuredtest/RestassuredTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llye.test.restassuredtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestassuredTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
